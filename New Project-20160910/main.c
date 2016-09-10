805b816e7427f3aeb3b6b23fc5822119291884c8
iGuilherme <iguilhermehfn@gmail.com>
2016-09-10 00:40:30 -0300

    Created by Coding Ground Client

---
+++ New Project-20160910/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+    int idade;
+    printf ("digite sua idade:");
+    scanf ("%d", idade);
+    
+    if (idade<18){
+        printf ("Menor de idade !");
+    }
+    else {
+    printf ("Maior de idade !");
+    }
+  
+      return 0;  
+    }
