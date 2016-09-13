886717cdd2e41e35a78602cc7b7d3b3f246bdc7b
iGuilherme <iguilhermehfn@gmail.com>
2016-09-12 21:47:45 -0300

    Created by Coding Ground Client

---
+++ New Project-20160913/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+    main nota1,nota2,nota3,nota4,media;
+    printf ("Digita sua nota 1 ");
+    scanft ("%d", nota 1);
+printf ("Digita sua nota 2");
+scanft ("%d", nota 2);
+printf ("Digita sua nota 3");
+scanft ("%d", nota  3);
+printf ("Digita sua nota 4");
+scanft ("%d", nota 4);
+media=((nota1*2)+(nota2*2)+(nota3*3)+(nota4*3)/10);
+if (media>=6) {
+printf ("Você foi aprovado");
+}
+else  {
+printf ("Você está de dependencia");
+}
+return 0;
+}
